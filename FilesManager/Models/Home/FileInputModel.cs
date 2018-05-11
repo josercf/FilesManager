67b6f06f1e07ba87142b18bbe843e5a996fa045b
José Romualdo da Costa Filho <jose.filho@contax.com.br>
2018-05-11 15:19:01 -0300

    Add project files.

---
+++ FilesManager/Models/Home/FileInputModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilesManager.Models.Home
+{
+    public class FileInputModel
+    {
+        public string Folder { get; set; }
+        public IFormFile File { get; set; }
+    }
+}
